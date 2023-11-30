2bb062dea3c25e99c5e92ff1f2ee07c35d14ddd5
Alec Sanders <aws022@bucknell.edu>
2023-11-30 13:32:38 -0500

    "Deliverables cont."

--- src/main/java/WalmartAPI/WalmartSearchView.java
+++ src/main/java/WalmartAPI/WalmartSearchView.java
@@ -177,7 +177,7 @@ public class WalmartSearchView extends Application {
         // Show placeholder label initially
         centerPane.setCenter(placeholderLabel);
 
-        return centerPane; 
+        return centerPane;
     }
 
     /**
